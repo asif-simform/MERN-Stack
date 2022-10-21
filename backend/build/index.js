a4031d881ce4f071a53801d6b7fca7bebe3113a6
Asif Vora <Asif.V@simformsolutions.com>
2022-10-21 18:00:39 +0530

    :sparkles: Setup node basic server

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('Hello world!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+var app = (0, express_1.default)();
+var port = process.env.PORT || 8080;
+app.get('/', function (req, res) {
+    res.send('Express + TypeScript Server');
+});
+app.listen(port, function () {
+    console.log("\u26A1\uFE0F[server]: Server is running at https://localhost:".concat(port));
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!')
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+
+const { PORT = 8080 } = process.env;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
