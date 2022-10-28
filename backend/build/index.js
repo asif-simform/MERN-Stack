077be2c7d408d79bb2e76f70f87b2eb895492811
Asif Vora <Asif.V@simformsolutions.com>
2022-10-28 15:12:49 +0530

    👷 Update CI build system

--- backend/build/index.js
+++ backend/build/index.js
@@ -7,10 +7,10 @@ var express_1 = __importDefault(require("express"));
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 var app = (0, express_1.default)();
-var _a = process.env.PORT, PORT = _a === void 0 ? 8080 : _a;
+var port = process.env.PORT || 8080;
 app.get('/', function (req, res) {
     res.send('Express + TypeScript Server');
 });
-app.listen(PORT, function () {
-    console.log("\u26A1\uFE0F[server]: Server is running at http://localhost:".concat(PORT));
+app.listen(port, function () {
+    console.log("\u26A1\uFE0F[server]: Server is running at http://localhost:".concat(port));
 });
