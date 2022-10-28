f4421b6d6f49157dfcc29b285e38d07d6fe0e861
Asif Vora <Asif.V@simformsolutions.com>
2022-10-28 17:51:55 +0530

    👷 Update CI build system

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
 import express, { Application, Request, Response } from 'express';
-import * as dotenv from "dotenv";
 
 declare var process : {
   env: {
