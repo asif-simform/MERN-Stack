9ec9d254bb4b90844bafa062582b77e8cbeaea51
Asif Vora <Asif.V@simformsolutions.com>
2022-11-01 12:15:46 +0530

    :heavy_plus_sign: Add a dependency.

--- backend/build/index.js
+++ backend/build/index.js
@@ -28,13 +28,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var dotenv = __importStar(require("dotenv"));
 dotenv.config();
-console.log("process:", process.env);
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var helmet_1 = __importDefault(require("helmet"));
 var app = (0, express_1.default)();
 var port = process.env.PORT || 8080;
+var isDevelopment = process.env.NODE_ENV === 'development';
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((0, helmet_1.default)());
+app.use((0, cors_1.default)());
+app.disable('x-powered-by');
 app.get('/', function (req, res) {
-    res.send('Express + TypeScript Server');
+    res.status(200).json({
+        message: 'Welcome to Node API\'s by 007',
+    });
 });
 app.listen(port, function () {
-    console.log("\u26A1\uFE0F[server]: Server is running at http://localhost:".concat(port));
+    console.log("\u26A1\uFE0F[server]: Server is running at ".concat(isDevelopment ? 'http://localhost:' : '').concat(port));
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,26 @@ import * as dotenv from 'dotenv';
 dotenv.config()
 
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app: Application = express();
+
 const port = process.env.PORT || 8080;
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cors());
+app.disable('x-powered-by');
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server 007');
+  res.status(200).json({
+    message: 'Welcome to Node API\'s by 007',
+  });
 });
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at ${isDevelopment ? 'http://localhost:' : ''}${port}`);
 });
