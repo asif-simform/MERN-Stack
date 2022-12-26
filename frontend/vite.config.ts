02e5e60537c0a1b40e3b8b7cf04f2ed09a1b86bb
Asif Vora <Asif.V@simformsolutions.com>
2022-12-26 15:25:13 +0530

    :wrench: build config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,15 +3,18 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    minify: true,
-    sourcemap: true,
-    rollupOptions: {
-      external: [],
-      output: {
-        globals: { },
-      },
-    },
+  // build: {
+  //   minify: true,
+  //   sourcemap: true,
+  //   rollupOptions: {
+  //     external: [],
+  //     output: {
+  //       globals: { },
+  //     },
+  //   },
+  // },
+  optimizeDeps: {
+    include: ['react', 'react-dom/client']
   },
   plugins: [react()],
 })
